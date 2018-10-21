cb64fe20a450d4e6e10b0b8dd51b0ab9ad045fbd
Marcos Loureiro <mloureiro.dev@gmail.com>
2018-10-21 15:56:04 +0200

    Focus data fetch and parse into the client itself

--- src/api.py
+++ src/api.py
@@ -1,36 +1,18 @@
 # -*- coding: utf-8 -*-
-import json
-from typing import Dict, NamedTuple
+from typing import Dict
 
 from src.client import CrowdinClient
 
 
-def process_results(result: Dict) -> Dict:
-    """
+def get_project_details(client: CrowdinClient) -> Dict:
+    result = client.get(method='info')
 
-    :param result:
-    :return:
-    """
-    response_json = json.loads(result.content.decode('utf-8'))
-    contents = {
-        'name': response_json.get("details").get("name"),
+    return {
+        'name': result.get("details").get("name"),
         'source':
-            response_json.get("details").get("source_language").get("code"),
+            result.get("details").get("source_language").get("code"),
         'languages':
-            [lang.get('code') for lang in response_json.get("languages")],
-        'last_build': response_json.get("details").get("last_build"),
-        'last_activity': response_json.get("details").get("last_activity")
+            [lang.get('code') for lang in result.get("languages")],
+        'last_build': result.get("details").get("last_build"),
+        'last_activity': result.get("details").get("last_activity")
     }
-
-    return contents
-
-
-def retrieve_info(args: NamedTuple) -> None:
-    """
-
-    :return:
-    """
-    client = CrowdinClient(
-        key=args.project_key, project=args.project_identifier)
-    results = client.get(method='info')
-    process_results(results)
